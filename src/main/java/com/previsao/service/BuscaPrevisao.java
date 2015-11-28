a7b0daa2306926ae161fbb351b67f8e320ba5f99
João Rodrigo Da Silva <joao.rodrigo@live.com>
2015-11-28 16:34:22 -0200

    Simplificando
    Simplificando o código, na primeira versão foi solicitado que fossem
    listadas a cidades e procurar por uma especifica(Florianópolis), somente
    após isto pegar a previsão.
    
    Simplificando o código agora. Pois como já sei qual cidade eu quero,
    basta ir buscar a previsão diretamente sem precisar listar cidades.

--- src/main/java/com/previsao/service/BuscaPrevisao.java
+++ src/main/java/com/previsao/service/BuscaPrevisao.java
@@ -15,99 +15,41 @@ import javax.xml.ws.Service;
 
 import com.previsao.ws.net.webservicex.GlobalWeatherSoap;
 import com.previsao.xmlClass.CurrentWeather;
-
+import java.io.StringReader;
 
 @Stateless
 public class BuscaPrevisao {
 
-	private static final String COUNTRY = "Brazil";
-	private static final String CITY = "Florianopolis";
+    private static final String COUNTRY = "Brazil";
+    private static final String CITY = "Florianopolis";
 
-	public CurrentWeather buscar() throws Exception {
-		try {
-			   URL wsdlURL = new URL("http://www.webservicex.net/globalweather.asmx?wsdl");
-			   QName serviceName = new QName("http://www.webserviceX.NET", "GlobalWeather");
-			   Service service = Service.create(wsdlURL, serviceName);
-			   GlobalWeatherSoap port = service.getPort(GlobalWeatherSoap.class);
-				//Set timeout until a connection is established
-			   ((BindingProvider) port).getRequestContext().put("javax.xml.ws.client.connectionTimeout", "15000");
+    public CurrentWeather buscar() throws Exception {
+        try {
+            URL wsdlURL = new URL("http://www.webservicex.net/globalweather.asmx?wsdl");
+            QName serviceName = new QName("http://www.webserviceX.NET", "GlobalWeather");
+            Service service = Service.create(wsdlURL, serviceName);
+            GlobalWeatherSoap port = service.getPort(GlobalWeatherSoap.class);
+            //Set timeout until a connection is established
+            ((BindingProvider) port).getRequestContext().put("javax.xml.ws.client.connectionTimeout", "15000");
 
-				//Set timeout until the response is received
-			   ((BindingProvider) port).getRequestContext().put("javax.xml.ws.client.receiveTimeout", "15000");			
-				
-				String cities = port.getCitiesByCountry(COUNTRY);
-				if (!cities.contains(CITY)) {
-					throw new Exception("Cidade não encontrada");
-				}
-				String previsao = getStringPrevisao(cities, port);
-				saveFile(previsao);
-				CurrentWeather c = unmarshalFilePrevisao(previsao);			
-			
-			
-			/*
-			GlobalWeather service = new GlobalWeather();
-			GlobalWeatherSoap port = service.getGlobalWeatherSoap();
-			//Set timeout until a connection is established
-			//((BindingProvider)service).getRequestContext().put("javax.xml.ws.client.connectionTimeout", "1000");
+            //Set timeout until the response is received
+            ((BindingProvider) port).getRequestContext().put("javax.xml.ws.client.receiveTimeout", "15000");
+            String previsao = port.getWeather(CITY, COUNTRY);
+            CurrentWeather c = unmarshalFilePrevisao(previsao);
+            return c;
+        } catch (javax.xml.ws.WebServiceException e) {
+            throw new Error("Servidor de previsão não respondendo, tente novamente mais tarde.", e);
+        }
+    }
 
-			//Set timeout until the response is received
-			//((BindingProvider) service).getRequestContext().put("javax.xml.ws.client.receiveTimeout", "1000");			
-			
-			String cities = port.getCitiesByCountry(COUNTRY);
-			if (!cities.contains(CITY)) {
-				throw new Exception("Cidade não encontrada");
-			}
-			String previsao = getStringPrevisao(cities, port);
-			saveFile(previsao);
-			CurrentWeather c = unmarshalFilePrevisao(previsao);
-			*/
-			return c;			
-		} catch (javax.xml.ws.WebServiceException e) {
-			throw new Error("Servidor de previsão não respondendo, tente novamente mais tarde.", e);
-		}
-	}
-	
-	private String getStringPrevisao(String cities, GlobalWeatherSoap port) {
-		try {
-			Integer idxBegin = cities.indexOf(CITY);
-			Integer idxEnd = cities.indexOf("<", idxBegin);
-			String cidade = cities.substring(idxBegin, idxEnd);
-			String previsao = port.getWeather(cidade, COUNTRY);
-			return previsao.replace("utf-16", "ISO-8859-1");     					
-		} catch (Exception e) {
-			throw new Error("Problemas ao separar previsão do tempo da cidade", e);
-		}
-	}
-
-	private CurrentWeather unmarshalFilePrevisao(String previsao) throws Exception {		
-		try {
-			File file = new File("previsao.xml");
-			JAXBContext context;
-			context = JAXBContext.newInstance("com.previsao.xmlClass");
-			Unmarshaller umn = context.createUnmarshaller();			
-			return (CurrentWeather) umn.unmarshal(file);
-		} catch (JAXBException e) {
-			throw new Exception("Problemas ao fazer Unmarshal do arquivo xml", e);
-		}	            
-	}
-	
-	private void saveFile(String previsao) throws Exception {
-		java.io.FileWriter fw = null; 
-		try {
-			fw = new java.io.FileWriter("previsao.xml");
-			fw.write(previsao);
-			fw.flush();
-		} catch (IOException e) {
-			throw new Exception("Problemas ao Salvar arquivo xml com previsão", e);
-		} finally {
-			if (!Objects.isNull(fw)) {
-				try {
-					fw.close();
-				} catch (IOException e) {
-					throw new Exception("Problemas ao fechar o arquivo xml", e);
-				}
-			}			
-		}
-	}
+    private CurrentWeather unmarshalFilePrevisao(String previsao) throws Exception {
+        try {
+            JAXBContext context = JAXBContext.newInstance(CurrentWeather.class);
+            Unmarshaller umn = context.createUnmarshaller();
+            return (CurrentWeather) umn.unmarshal(new StringReader(previsao));
+        } catch (JAXBException e) {
+            throw new Exception("Problemas ao fazer Unmarshal do arquivo xml", e);
+        }
+    }
 
 }
